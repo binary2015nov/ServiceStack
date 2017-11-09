ae0e0fdcfc162f1cbdfae3758116dddcb6706fe4
申行 <binary2015nov@outlook.com>
2017-11-09 17:45:51 +0800

    update dlls

--- src/ServiceStack.Common/Support/AdapterBase.cs
+++ src/ServiceStack.Common/Support/AdapterBase.cs
@@ -20,21 +20,21 @@ namespace ServiceStack.Support
         {
             DateTime before = DateTime.UtcNow;
 #if !NETFX_CORE && !WP
-            Logger.Debug($"Executing action '{action.Method().Name}'");
+            Logger.Debug($"Executing action '{action.Method.Name}'");
 #endif
             try
             {
                 T result = action();
                 TimeSpan timeTaken = DateTime.UtcNow - before;
 #if !NETFX_CORE && !WP
-                Logger.Debug($"Action '{action.Method().Name}' executed. Took {timeTaken.TotalMilliseconds} ms.");
+                Logger.Debug($"Action '{action.Method.Name}' executed. Took {timeTaken.TotalMilliseconds} ms.");
 #endif
                 return result;
             }
             catch (Exception ex)
             {
 #if !NETFX_CORE && !WP
-                Logger.Error($"There was an error executing Action '{action.Method().Name}'. Message: {ex.Message}");
+                Logger.Error($"There was an error executing Action '{action.Method.Name}'. Message: {ex.Message}");
 #endif
                 throw;
             }
@@ -48,20 +48,20 @@ namespace ServiceStack.Support
         {
             DateTime before = DateTime.UtcNow;
 #if !NETFX_CORE && !WP
-            Logger.Debug($"Executing action '{action.Method().Name}'");
+            Logger.Debug($"Executing action '{action.Method.Name}'");
 #endif
             try
             {
                 action();
                 TimeSpan timeTaken = DateTime.UtcNow - before;
 #if !NETFX_CORE && !WP
-                Logger.Debug($"Action '{action.Method().Name}' executed. Took {timeTaken.TotalMilliseconds} ms.");
+                Logger.Debug($"Action '{action.Method.Name}' executed. Took {timeTaken.TotalMilliseconds} ms.");
 #endif
             }
             catch (Exception ex)
             {
 #if !NETFX_CORE && !WP
-                Logger.Error($"There was an error executing Action '{action.Method().Name}'. Message: {ex.Message}");
+                Logger.Error($"There was an error executing Action '{action.Method.Name}'. Message: {ex.Message}");
 #endif
                 throw;
             }
